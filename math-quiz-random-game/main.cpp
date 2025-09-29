cf2127b999ca87e497b3350f065855344078a2f0
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-09-29 13:19:20 +0300

    Update main.cpp

--- math-quiz-random-game/main.cpp
+++ math-quiz-random-game/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <random>
-#include <ctime>
 
 
 using namespace std;
@@ -15,8 +14,39 @@ int main()
 
     int n = distrib(gen);
     int m = distrib(gen);
-    
-    cout << n << endl;
+    int score = 0;
+    char operations[4] = {'+', '-', '*', '/'};
+
+
+    char operation = '+';
+    int answer = 0;
+
+    switch (operation) {
+        case "+":
+            answer = n + m;
+            break;
+        case "-":
+            answer = n - m;
+            break;
+        case "*":
+            answer = n * m;
+            break;
+        case "/":
+            answer = n / m;
+            break;
+        default:
+        cout << "Invalid operation." << endl;
+    }
+    int given_answer;
+    while (true)
+        cin >> given_answer;
+        if (given_answer == answer) {
+            score += given_answer;
+        }
+
+
+
+    }
     return 0;
 }
 
